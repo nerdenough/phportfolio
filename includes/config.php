8c950ad451cb86699232be0787a0b52634787dfc
Brendan Goodenough <brendan.goodenough.nz@gmail.com>
2014-10-18 10:11:34 +1300

    Adjustment of minor config defaults

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
 // MySQL Config
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'phportfolio');
-define('DB_PASSWORD', 'bodacious');
+define('DB_USERNAME', 'user');
+define('DB_PASSWORD', 'pass');
 define('DB_NAME', 'phportfolio');
 define('DB_PREFIX', 'phportfolio_')
 ?>
